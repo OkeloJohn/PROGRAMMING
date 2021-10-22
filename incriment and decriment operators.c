1cd1511b26c961726f65c0dd1a560051f09fc690
OkeloJohn <91739446+OkeloJohn@users.noreply.github.com>
2021-10-22 12:06:18 +0300

    Update incriment and decriment operators.c

--- incriment and decriment operators.c
+++ incriment and decriment operators.c
@@ -1,14 +1,15 @@
-// Working of increment and decrement operators
-#include <stdio.h>
+/*increment and decriment operator working*/
+#include<stdio.h>
 int main()
+
 {
-    int a = 10, b = 100;
-    float c = 10.5, d = 100.5;
-
-    printf("++a = %d \n", ++a);
-    printf("--b = %d \n", --b);
-    printf("++c = %f \n", ++c);
-    printf("--d = %f \n", --d);
-
+    int p=13;
+    int q=20;
+    float r=20.55;
+    float s=100.01;
+    printf("p=%d",++p);
+    printf("q=%d",--q);
+    printf("r=%f",++r);
+    printf("s=%f",--s);
     return 0;
 }
