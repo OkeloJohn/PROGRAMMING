46bbf5e276fc55d92e0f8ab0abbea03c3ae47f6c
OkeloJohn <91739446+OkeloJohn@users.noreply.github.com>
2021-10-22 11:23:04 +0300

    Update assignment operators.c

--- assignment operators.c
+++ assignment operators.c
@@ -1,35 +1,18 @@
-// C program to demonstrate
-// working of Assignment operators
-  
-#include <stdio.h>
-  
+/*asignment operators*/
+#include<stdio.h>
+
 int main()
+
 {
-  
-    // Assigning value 10 to a
-    // using "=" operator
-    int a = 10;
-    printf("Value of a is %d\n", a);
-  
-    // Assigning value by adding 10 to a
-    // using "+=" operator
-    a += 10;
-    printf("Value of a is %d\n", a);
-  
-    // Assigning value by subtracting 10 from a
-    // using "-=" operator
-    a -= 10;
-    printf("Value of a is %d\n", a);
-  
-    // Assigning value by multiplying 10 to a
-    // using "*=" operator
-    a *= 10;
-    printf("Value of a is %d\n", a);
-  
-    // Assigning value by dividing 10 from a
-    // using "/=" operator
-    a /= 10;
-    printf("Value of a is %d\n", a);
-  
+    int b=20;
+    printf("value of b is %d/n",b);
+    b+=20;
+    printf("value of b is %d/n",b);
+    b-=20;
+    printf("value of b is %d/n",b);
+    b*=20;
+    printf("value of b is %d/n",b);
+    b/=20;
+    printf("value of b is %d/n",b);
     return 0;
 }
