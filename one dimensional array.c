e65d6f399b93e31e007c80745e3ee121bdc81f58
OkeloJohn <91739446+OkeloJohn@users.noreply.github.com>
2021-11-16 17:13:43 +0300

    Create one dimensional array.c

---
+++ one dimensional array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a[5]= {10,20,30,40,50}; //using compile time initializaton
+int i;
+printf("elements_of array_are:");
+for(i=0;i<5;i++)
+printf("%d/n", a[i]);
+}
